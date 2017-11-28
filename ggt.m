f98e4043849a99eafbd057e790908184b3253fc5
Sepehr Laal <sepehr.laal@gmail.com>
2017-11-28 10:37:28 -0800

    also put matlab's ransac estimates in cache for plotting

--- ggt.m
+++ ggt.m
@@ -42,18 +42,23 @@ while(hasFrame(vr))
     % checkerboard 3d points
     entry.WorldPoints = zeros(prod(entry.BoardSize - 1),3);
     entry.WorldPoints(:,1:2) = generateCheckerboardPoints(entry.BoardSize, 30); % board's blocks are 30mm
-    % camera's extrinsics which represent the coordinate system transformation from world coordinates to camera coordinates.
-    [entry.RotationExtrinsics, entry.TranslationExtrinsics] = ...
+    % camera's extrinsics which represent the coordinate system transformation from world coordinates to camera coordinates (true).
+    [entry.ExtrinsicsRotationTrue, entry.ExtrinsicsTranslationTrue] = ...
         extrinsics(entry.ImagePoints, entry.WorldPoints(:,1:2), cameraParams);
     % camera's orientation and location, represent the 3-D camera pose in the world coordinates
-    [entry.Rotation, entry.Translation] = ...
-        extrinsicsToCameraPose(entry.RotationExtrinsics, entry.TranslationExtrinsics);
+    [entry.PoseRotationTrue, entry.PoseTranslationTrue] = ...
+        extrinsicsToCameraPose(entry.ExtrinsicsRotationTrue, entry.ExtrinsicsTranslationTrue);
+    % camera's extrinsics which represent the coordinate system transformation from world coordinates to camera coordinates (estimate).
+    [pR,pt] = estimateWorldCameraPose(entry.ImagePoints, entry.WorldPoints, cameraParams);
+    [entry.ExtrinsicsRotationEst, entry.ExtrinsicsTranslationEst] = cameraPoseToExtrinsics(pR, pt);
     % convert to quaternions, since we estimate quaternions in this implementation
-    entry.Rotation = rotm2quat(entry.Rotation)';
-    entry.RotationExtrinsics = rotm2quat(entry.RotationExtrinsics)';
+    entry.ExtrinsicsRotationTrue = rotm2quat(entry.ExtrinsicsRotationTrue)';
+    entry.ExtrinsicsRotationEst = rotm2quat(entry.ExtrinsicsRotationEst)';
+    entry.PoseRotationTrue = rotm2quat(entry.PoseRotationTrue)';
     % we use Nx1 sizes for everything by convention in this implementation
-    entry.Translation = entry.Translation';
-    entry.TranslationExtrinsics = entry.TranslationExtrinsics';
+    entry.ExtrinsicsTranslationTrue = entry.ExtrinsicsTranslationTrue';
+    entry.ExtrinsicsTranslationEst = entry.ExtrinsicsTranslationEst';
+    entry.PoseTranslationTrue = entry.PoseTranslationTrue';
     out = [out entry];
     index = index + 1;
 end
