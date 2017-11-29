d4463bd4eaa3848da79e9af0a55dff9522c6ff91
Sepehr Laal <sepehr.laal@gmail.com>
2017-11-29 14:23:47 -0800

    changed estimation method to weighted mean
    + moved createNoise for ease of editting
    + fixed policy checking
    + fine tuned quaternion noise params

--- main.m
+++ main.m
@@ -9,10 +9,10 @@ if ~libisloaded('p2c'); loadlibrary p2c; end;
 N = 1000000; % particle count
 input = 'data/pixel-xl.mp4'; % video file
 policy = 'gpu'; % 'gpu', 'cpu', or 'matlab'
-positionSigma = [100 100 100]; % mm, in XYZ axes
-rotationSigma = 0.02; % radians, in Euler axes
+positionSigma = [100 200 100]; % mm, in XYZ axes
+rotationSigma = [0.1 0.05 0.5 1]; % unitless
 groundTruth = ggt(input); % ground truth 2d and 3d data
-[positionNoise, rotationNoise] = grn(N, positionSigma, rotationSigma);
+[positionNoise, rotationNoise] = createNoise(N, positionSigma, rotationSigma);
 frameCount = length(groundTruth); % this is how many total frames we have
 load('cameraParams.mat'); % previously calculated camera intrinsics (K-mat)
 
@@ -48,7 +48,6 @@ while index <= length(groundTruth) && hasFrame(vr)
         % second frame onward is for tracking (localization)
         System.xp = updateParticles(System,positionNoise,rotationNoise);
         System.wp = updateWeights(System,cameraParams,policy);
-        effective = 1/sum(System.wp.^2);
         System.xp = resampleParticles(System);
         [q,t] = extractEstimates(System);
         plotSystem(vw,cameraParams,System,frame,entry,t,q,index);
@@ -63,14 +62,14 @@ close(vw);
 % _________________________________________________________________________
 % Extratcs the mean of particles as system's estimate.
 function [q,t] = extractEstimates(System)
-    q = mean(System.xp(:,4:7)); % estimated quaternion
-    t = mean(System.xp(:,1:3)); % estimated translation
+    q = sum(System.wp .* System.xp(:,4:7)); % estimated quaternion
+    t = sum(System.wp .* System.xp(:,1:3)); % estimated translation
     [q,t] = cameraPoseToExtrinsics(quat2rotm(q),t);
     q = rotm2quat(q)';
 end
 
 % Systematic resmapling of particles.
-function [xp,wp] = resampleParticles(System)
+function xp = resampleParticles(System)
     R = cumsum(System.wp);
     T = rand(1, System.N);
     [~, I] = histc(T, R);
@@ -84,10 +83,10 @@ end
 
 % Updates particle weights based on their liklihood (measurement model).
 function wp = updateWeights(System,cameraParams,policy)
-    if policy == 'gpu'
+    if isequal(policy, 'gpu')
         % Execude on a Cuda capable device (GPU)
         wp = nativeUpdateWeightsGpu(System);
-    elseif policy == 'cpu'
+    elseif isequal(policy, 'cpu')
         % Execude with native code (C++11 runtime)
         wp = nativeUpdateWeightsCpu(System);
     else
@@ -114,7 +113,7 @@ function wp = updateWeights(System,cameraParams,policy)
                 D(j) = (d/C)*d';
             end
 
-            System.wp(i) = (1/(2*pi*sqrt(det(C))))*exp(-sum(D)/2);
+            System.wp(i) = System.wp(i) * (1/(2*pi*sqrt(det(C))))*exp(-sum(D)/2);
         end
         wp = System.wp ./ sum(System.wp);        
     end
@@ -137,4 +136,17 @@ function [xp,wp] = createParticles(q0,t0,N)
     xp(:,6) = q0(3); % q-j
     xp(:,7) = q0(4); % q-k
     wp = ones(N,1) / N;
+end
+
+% genrate Random Noise.
+function [positionNoise, rotationNoise] = createNoise(N, positionSigma, rotationSigma)
+    assert(isequal(size(positionSigma),[1,3]), 'position standard deviation msut be 3x1');
+    assert(isequal(size(rotationSigma),[1 4]), 'rotation standard deviation msut be 1x4');
+    v = rotationSigma(2:4) .* randn(N,3);
+    r = rotationSigma(1) * randn(N,1);
+    rotationNoise = zeros(N,4);
+    for i=1:N
+        rotationNoise(i,:) = [cos(r(i)) sin(r(i)) * v(i,:) / norm(v(i,:))]';
+    end
+    positionNoise = repmat(positionSigma, N, 1) .* randn(N,3);
 end
